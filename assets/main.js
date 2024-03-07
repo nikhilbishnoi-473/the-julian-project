84a640424f2c1228ba23db55cce98c654af2131a
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-03-06 14:40:19 -1200

    update done

--- assets/main.js
+++ assets/main.js
@@ -58,3 +58,19 @@ function  layout()
     document.getElementById("preloader").classList.remove("image")
     document.getElementById("img").style.display ="none"
 }
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
